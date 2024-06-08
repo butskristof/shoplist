07adf7d2ec185a6523ab6a8cab4a57fc3d2846c5
Kristof Buts <kristof.buts@euri.com>
2024-06-08 13:01:32 +0200

    working app without layout

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,7 +1,6 @@
 import '@mantine/core/styles.css';
 import { Button, MantineProvider } from '@mantine/core';
 import { useState } from 'react';
-import AppLayout from './components/layout/app-layout/AppLayout.tsx';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,20 +8,10 @@ function App() {
 
   return (
     <MantineProvider>
-      <AppLayout>
-        <div
-          className="counter"
-          style={{ display: 'none' }}
-        >
-          <p>Current count: {count}</p>
-          <Button onClick={increment}>Increment</Button>
-          {/* eslint-disable-next-line no-constant-binary-expression */}
-          {false &&
-            Array(100)
-              .fill('some content')
-              .map((c, i) => <p key={i}>{c}</p>)}
-        </div>
-      </AppLayout>
+      <div className="counter">
+        <p>Current count: {count}</p>
+        <Button onClick={increment}>Increment</Button>
+      </div>
     </MantineProvider>
   );
 }
