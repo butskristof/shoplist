50f5ee91c54ec1a078d74a69954a7d07f31bb9b7
Kristof Buts <git@kristofbuts.be>
2024-06-11 16:05:47 +0200

    back to overview link

--- frontend/app/src/pages/lists/ListPage.tsx
+++ frontend/app/src/pages/lists/ListPage.tsx
@@ -1,10 +1,11 @@
 import { Link, useParams } from 'react-router-dom';
 import { Button } from '@mantine/core';
-import { IoHome } from 'react-icons/io5';
+import { IoArrowBack, IoHome } from 'react-icons/io5';
 import { useShoplistsApiGetList } from '@/data/shoplists-api.ts';
 import NotFound from '@/components/common/NotFound.tsx';
 import EntityLoader from '@/components/common/EntityLoader.tsx';
 import Shoplist from '@/components/lists/detail/Shoplist.tsx';
+import classes from './ListPage.module.scss';
 
 const ListPage = () => {
   // we assume id will be available, otherwise the router
@@ -12,26 +13,37 @@ const ListPage = () => {
   const { id } = useParams() as { id: string };
   const query = useShoplistsApiGetList(id);
 
-  if (query.isPending) return <EntityLoader entity="list" />;
-
-  if (query.isError && query.error.response.status === 404)
-    return (
-      <NotFound
-        entity="list"
-        actions={
-          <>
-            <Button
-              component={Link}
-              to="/lists"
-              leftSection={<IoHome />}
-            >
-              Back to list overview
-            </Button>
-          </>
-        }
-      />
-    );
-
-  return <Shoplist list={query.data} />;
+  return (
+    <>
+      <div className={classes.header}>
+        <Button
+          component={Link}
+          to="/lists"
+          leftSection={<IoArrowBack />}
+          variant="light"
+        >
+          Back to overview
+        </Button>
+      </div>
+      {query.isPending && <EntityLoader entity="list" />}
+      {query.isError && query.error.response.status === 404 && (
+        <NotFound
+          entity="list"
+          actions={
+            <>
+              <Button
+                component={Link}
+                to="/lists"
+                leftSection={<IoHome />}
+              >
+                Back to list overview
+              </Button>
+            </>
+          }
+        />
+      )}
+      {query.isSuccess && <Shoplist list={query.data} />}
+    </>
+  );
 };
 export default ListPage;
