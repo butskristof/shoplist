a1a0669f1db34bb8ef009b62ed1c1e869767cf3a
Kristof Buts <git@kristofbuts.be>
2024-06-11 22:14:09 +0200

    add header

--- frontend/app/src/components/common/LeftRightHeader.tsx
+++ frontend/app/src/components/common/LeftRightHeader.tsx
@@ -1,15 +1,21 @@
 import { FC, ReactNode } from 'react';
+import clsx from 'clsx';
 import classes from './LeftRightHeader.module.scss';
+import { PropsWithClassName } from '@/types/PropsWithClassName.ts';
 
-interface Props {
+interface Props extends PropsWithClassName {
   left?: ReactNode;
   right?: ReactNode;
 }
-const LeftRightHeader: FC<Props> = ({ left, right }) => (
-  <div className={classes.header}>
-    {left != null ? left : <div></div>}
-    {right != null ? right : <div></div>}
-  </div>
-);
+const LeftRightHeader: FC<Props> = ({ left, right, className }) => {
+  const headerClassNames = clsx(classes.header, className);
+
+  return (
+    <div className={headerClassNames}>
+      {left != null ? left : <div className={classes.left}></div>}
+      {right != null ? right : <div className={classes.right}></div>}
+    </div>
+  );
+};
 
 export default LeftRightHeader;
