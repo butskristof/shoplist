61330170da07c2f4b5f6fa8d05d7207546a8b73b
Kristof Buts <git@kristofbuts.be>
2024-06-11 10:08:49 +0200

    basic layout

--- frontend/app/src/components/app/AppLayout.tsx
+++ frontend/app/src/components/app/AppLayout.tsx
@@ -1,3 +1,21 @@
-const AppLayout = ({ children }) => children;
+import { FC } from 'react';
+import { PropsWithChildren } from '@/types/PropsWithChildren.ts';
+import classes from './AppLayout.module.scss';
+
+const AppLayout: FC<PropsWithChildren> = ({ children }) => (
+  <div className={classes.layout}>
+    <div className={classes.header}>header</div>
+    <div className={classes.body}>
+      <div className={classes.page}>{children}</div>
+      <div className={classes.footer}>
+        <div className={classes.actions}>
+          <div>action</div>
+          <div>action</div>
+          <div>action</div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default AppLayout;
