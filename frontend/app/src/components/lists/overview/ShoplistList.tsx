69d566d43ab7fb73c4341c89324d0caea60a6cbb
Kristof Buts <git@kristofbuts.be>
2024-06-11 13:21:59 +0200

    set up components

---
+++ frontend/app/src/components/lists/overview/ShoplistList.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface Props {
+  lists: never[];
+  loading: boolean;
+}
+const ShoplistList: FC<Props> = () => <div></div>;
+
+export default ShoplistList;
--- frontend/app/src/pages/lists/ListsPage.tsx
+++ frontend/app/src/pages/lists/ListsPage.tsx
@@ -2,6 +2,7 @@ import { Button } from '@mantine/core';
 import { IoAdd } from 'react-icons/io5';
 import LeftRightHeader from '@/components/common/LeftRightHeader.tsx';
 import classes from './ListsPage.module.scss';
+import ShoplistList from '@/components/lists/overview/ShoplistList.tsx';
 
 const ListsPage = () => (
   <div>
@@ -12,6 +13,10 @@ const ListsPage = () => (
           <Button leftSection={<IoAdd size={20} />}>Add new list</Button>
         </div>
       }
+    />
+    <ShoplistList
+      lists={[]}
+      loading={false}
     />
   </div>
 );
