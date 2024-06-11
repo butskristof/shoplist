4f42d7f4462225e7ce15cbf8aa670d087967b0a6
Kristof Buts <git@kristofbuts.be>
2024-06-11 13:13:11 +0200

    header & add button

--- frontend/app/src/pages/lists/ListsPage.tsx
+++ frontend/app/src/pages/lists/ListsPage.tsx
@@ -1,20 +1,18 @@
-import { Link } from 'react-router-dom';
-import { useShoplistsApiLists } from '@/hooks/api/shoplists-api.ts';
+import { Button } from '@mantine/core';
+import { IoAdd } from 'react-icons/io5';
+import LeftRightHeader from '@/components/common/LeftRightHeader.tsx';
+import classes from './ListsPage.module.scss';
 
-const ListsPage = () => {
-  const { data: lists = [] } = useShoplistsApiLists();
-
-  return (
-    <div>
-      <h1>Lists</h1>
-      <ul>
-        {lists.map((l) => (
-          <li key={l.id}>
-            <Link to={`/lists/${l.id}`}>{l.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+const ListsPage = () => (
+  <div>
+    <LeftRightHeader
+      left={<h1>Lists</h1>}
+      right={
+        <div className={classes['header-actions']}>
+          <Button leftSection={<IoAdd size={20} />}>Add new list</Button>
+        </div>
+      }
+    />
+  </div>
+);
 export default ListsPage;
