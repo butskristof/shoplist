3da32e9e272f58223ebcc07198b1e25959911c34
Kristof Buts <git@kristofbuts.be>
2024-06-11 16:43:21 +0200

    working toggle

--- frontend/app/src/services/shoplists-api.ts
+++ frontend/app/src/services/shoplists-api.ts
@@ -1,10 +1,12 @@
 import { createAxiosInstance } from './base-api.ts';
-import { Shoplist, ShoplistWithItems } from '@/types/shoplists-api.types.ts';
+import { Shoplist, ShoplistItem, ShoplistWithItems } from '@/types/shoplists-api.types.ts';
 
 const api = createAxiosInstance('/api');
 
 export const ShoplistsApi = {
   getLists: () => api.get<Shoplist[]>('/lists').then((r) => r.data),
   getList: (id: string) =>
-    api.get<ShoplistWithItems | null>(`/lists/${id}?_embed=items`).then((r) => r.data),
+    api.get<ShoplistWithItems>(`/lists/${id}?_embed=items`).then((r) => r.data),
+  updateItem: (payload: ShoplistItem) =>
+    api.put<ShoplistItem>(`items/${payload.id}`, payload).then((r) => r.data),
 };
