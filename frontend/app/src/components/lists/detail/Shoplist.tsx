0f65f3c56211e53706354717611d69c34b0d36bb
Kristof Buts <git@kristofbuts.be>
2024-06-12 11:42:08 +0200

    Create new shoplist item (#8)
    - Create new Shoplist item 
    - Add form validation w/ react-hook-form & Mantine adapter
    - Use modals

--- frontend/app/src/components/lists/detail/Shoplist.tsx
+++ frontend/app/src/components/lists/detail/Shoplist.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import { Button } from '@mantine/core';
 import { IoAdd } from 'react-icons/io5';
+import { useDisclosure } from '@mantine/hooks';
 import { ShoplistWithItems } from '@/types/shoplists-api.types.ts';
 import ShoplistItem from '@/components/lists/detail/ShoplistItem.tsx';
 import classes from './Shoplist.module.scss';
 import LeftRightHeader from '@/components/common/LeftRightHeader.tsx';
+import CreateShoplistItem from '@/components/lists/detail/CreateShoplistItem.tsx';
 
 interface Props {
   list: ShoplistWithItems;
@@ -15,8 +17,10 @@ const Shoplist: FC<Props> = ({ list }) => {
   const sortedItems = list.items.toSorted((a) => (a.ticked ? 1 : -1));
   const untickedCount = list.items.filter((i) => !i.ticked).length;
 
+  const [showCreate, { open: openCreate, close: closeCreate }] = useDisclosure(false);
+
   return (
-    <div>
+    <div className={classes.shoplist}>
       <LeftRightHeader
         className={classes.header}
         left={
@@ -29,10 +33,21 @@ const Shoplist: FC<Props> = ({ list }) => {
         }
         right={
           <div>
-            <Button leftSection={<IoAdd size={20} />}>Add item</Button>
+            <Button
+              leftSection={<IoAdd size={20} />}
+              onClick={openCreate}
+            >
+              Add item
+            </Button>
           </div>
         }
       />
+      {showCreate && (
+        <CreateShoplistItem
+          listId={list.id}
+          onClose={closeCreate}
+        />
+      )}
       <div className={classes.list}>
         {sortedItems.map((i) => (
           <ShoplistItem
@@ -44,4 +59,5 @@ const Shoplist: FC<Props> = ({ list }) => {
     </div>
   );
 };
+
 export default Shoplist;
--- frontend/app/src/services/shoplists-api.ts
+++ frontend/app/src/services/shoplists-api.ts
@@ -5,8 +5,13 @@ const api = createAxiosInstance('/api');
 
 export const ShoplistsApi = {
   getLists: (): Promise<Shoplist[]> => api.get<Shoplist[]>('/lists').then((r) => r.data),
+
   getList: (id: string): Promise<ShoplistWithItems> =>
     api.get<ShoplistWithItems>(`/lists/${id}?_embed=items`).then((r) => r.data),
+
+  createItem: (payload: ShoplistItem): Promise<ShoplistItem> =>
+    api.post<ShoplistItem>('items', payload).then((r) => r.data),
+
   updateItem: (payload: ShoplistItem): Promise<ShoplistItem> =>
     api.put<ShoplistItem>(`items/${payload.id}`, payload).then((r) => r.data),
 };
