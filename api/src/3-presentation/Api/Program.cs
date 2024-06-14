1bb166690dd98a0879c28e6c5cb8baaf83166381
Kristof Buts <git@kristofbuts.be>
2024-06-14 15:06:20 +0200

    Solution & projects

---
+++ api/src/3-presentation/Api/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+}
+
+app.Run();
