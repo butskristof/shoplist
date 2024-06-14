527c73a8ccb91f8ecac46fcff2b31aff0b55b262
Kristof Buts <git@kristofbuts.be>
2024-06-14 16:29:42 +0200

    working create

--- api/src/1-core/Application/Modules/Lists/CreateList.cs
+++ api/src/1-core/Application/Modules/Lists/CreateList.cs
@@ -3,6 +3,8 @@ using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Shoplists.Application.Common.FluentValidation;
+using Shoplists.Application.Common.Persistence;
+using Shoplists.Domain.Models;
 
 namespace Shoplists.Application.Modules.Lists;
 
@@ -31,18 +33,31 @@ public static class CreateList
         #region construction
 
         private readonly ILogger<Handler> _logger;
+        private readonly IAppDbContext _db;
 
-        public Handler(ILogger<Handler> logger)
+        public Handler(ILogger<Handler> logger, IAppDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         #endregion
 
-        public Task<ErrorOr<Response>> Handle(Request request, CancellationToken cancellationToken)
+        public async Task<ErrorOr<Response>> Handle(Request request, CancellationToken cancellationToken)
         {
             _logger.LogDebug("Creating a new List");
-            throw new NotImplementedException();
+
+            var list = new List { Name = request.Name };
+            _logger.LogDebug("Mapped request to entity");
+
+            _db.Lists.Add(list);
+            await _db.SaveChangesAsync(CancellationToken.None);
+            _logger.LogDebug("Persisted new entity to database");
+
+            var response = new Response(list.Id, list.Name);
+            _logger.LogDebug("Mapped entity to response DTO");
+            
+            return response;
         }
     }
 }
