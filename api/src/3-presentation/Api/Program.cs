89dd1c715afc72d169e10502d821abb6891b7760
Kristof Buts <git@kristofbuts.be>
2024-06-16 13:25:01 +0200

    migrate to serilog & automatic migrations

--- api/src/3-presentation/Api/Program.cs
+++ api/src/3-presentation/Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog;
 using Shoplists.Api;
 using Shoplists.Api.Modules;
 using Shoplists.Application;
@@ -5,38 +7,78 @@ using Shoplists.Application.Common.Constants;
 using Shoplists.Infrastructure;
 using Shoplists.Persistence;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .CreateBootstrapLogger();
 
-builder.Services
-    .AddConfiguration()
-    .AddApplication()
-    .AddInfrastructure()
-    .AddPersistence(
-        builder.Configuration.GetConnectionString(ConfigurationConstants.AppDbContextConnectionStringKey)
-    )
-    .AddApi();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+    builder.Services
+        .AddConfiguration()
+        .AddApplication()
+        .AddInfrastructure()
+        .AddPersistence(
+            builder.Configuration.GetConnectionString(ConfigurationConstants.AppDbContextConnectionStringKey)
+        )
+        .AddApi();
 
-app
-    // the default exception handler will catch unhandled exceptions and return 
-    // them as ProblemDetails with status code 500 Internal Server Error
-    .UseExceptionHandler()
-    // the status code pages will map additional failed requests (outside of
-    // those throwing exceptions) to responses with ProblemDetails body content
-    // this includes 404, method not allowed, ... (all status codes between 400 and 599)
-    // keep in mind that this middleware will only activate if the body is empty when
-    // it reaches it
-    .UseStatusCodePages();
+    builder.Host
+        .UseSerilog((context, configuration) => configuration
+            .Enrich.FromLogContext()
+            .ReadFrom.Configuration(context.Configuration)
+        );
 
-app.UseAuthorization();
+    var app = builder.Build();
 
-app.MapHealthChecks("/health");
-app
-    .MapSwagger()
-    .RequireAuthorization();
-app
-    .MapListsEndpoints()
-    .MapListItemsEndpoints();
+    app
+        // the default exception handler will catch unhandled exceptions and return 
+        // them as ProblemDetails with status code 500 Internal Server Error
+        .UseExceptionHandler()
+        // the status code pages will map additional failed requests (outside of
+        // those throwing exceptions) to responses with ProblemDetails body content
+        // this includes 404, method not allowed, ... (all status codes between 400 and 599)
+        // keep in mind that this middleware will only activate if the body is empty when
+        // it reaches it
+        .UseStatusCodePages();
 
-app.Run();
+    app.UseAuthorization();
+
+    app.MapHealthChecks("/health");
+    app
+        .MapSwagger()
+        .RequireAuthorization();
+    app
+        .MapListsEndpoints()
+        .MapListItemsEndpoints();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        try
+        {
+            logger.LogInformation("Migrating application database");
+            
+            var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            context.Database.Migrate();
+            
+            logger.LogInformation("Migrated application database");
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Failed to migrate application database: {Message}", ex.Message);
+            throw;
+        }
+    }
+    
+    app.Run();
+}
+catch (Exception ex) when (ex is not HostAbortedException)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+    throw;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
